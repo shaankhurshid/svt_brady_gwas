df622497856190e00a5e52a337ac67893a1c2d6d
sk259 <skhurshid@partners.org>
2020-03-25 19:22:20 -0400

    cleaned up for speed?

--- gather_phenotype.R
+++ gather_phenotype.R
@@ -7,47 +7,28 @@ library(data.table)
 ## Load data
 data <- fread(file='/mnt/ml4cvd/projects/skhurshid/svt_brady_gwas/phenotypes_long.csv',data.table=F)
 
-## Specify how many columns exist prior to "disease" (so they are preserved)
-lead_columns <- 2
+## Reduce columns to sample_id + phenotypes only
+data[,-c('enroll_age')]
 
 ## Create function to convert to standard wide format
 widen <- function(data){
-  for (i in unique(data[,'disease'])){
-    data[,paste0('incident',i)] <- rep(NA,nrow(data))
-    data[,paste0('prevalent',i)] <- rep(NA,nrow(data))
-    data[,paste0('survyears',i)] <- rep(NA,nrow(data))}
-  for (i in 1:nrow(data)){
-    data[,paste0('incident',data[,'disease'][i])][i] <- data[,'incident_disease'][i]
-    data[,paste0('prevalent',data[,'disease'][i])][i] <- data[,'prevalent_disease'][i]
-    data[,paste0('survyears',data[,'disease'][i])][i] <- data[,'survyears_disease'][i]
-    if (i %% 1000 == 0){
-    print(paste0('I am working on row # ',i,' out of ',nrow(data),' rows!'))}
+  out <- list()
+  n <- 0
+  for (i in unique(data[,'sample_id'])){
+    n <- n + 1
+    subset <- data[data[,'sample_id']==i,]
+    out[[n]] <- data.frame(matrix(ncol=length(unique(subset[,'disease']))*3+1,nrow=0))
+    out[[n]][1,1] <- i; names(out[[n]])[1] <- 'sample_id'
+    m <- 2
+    for (j in unique(subset[,'disease'])){
+      out[[n]][1,m:(m+2)] <- c(subset[subset[,'disease']==j,'prevalent_disease'],
+                               subset[subset[,'disease']==j,'incident_disease'],
+                               subset[subset[,'disease']==j,'survyears_disease'])
+      names(out[[n]])[m:(m+2)] <- c(paste0('prev_',j),paste0('incd_',j),paste0('survyears_',j))
+      m <- m+3
+    }
   }
-  return(data)
+  return(do.call(rbind,out))
 }
-
-## Convert
-data <- widen(data)
-
-## Back to DT
-setDT(data)
-
-## Apply max function to get unique column values
-## List of column names
-out_list <- list()
-i <- 1
-while (i <= length(unique(data$disease))){
-  out_list[[length(out_list)+1]] <- paste0('incident_',unique(data$disease)[i])
-  out_list[[length(out_list)+1]] <- paste0('prevalent_',unique(data$disease)[i])
-  out_list[[length(out_list)+1]] <- paste0('survyears_',unique(data$disease)[i])
-  i <- i+1
-}
-
-out_list <- unlist(out_list)
-
-c[,(out_list) := (lapply(.SD,max,na.rm=T)),
-  .SDcols=names(data)[(lead_columns+1):length(names(data))],by=sample_id]
-
-data <- unique(data,by='sample_id')
 
 save(data,file='/mnt/ml4cvd/projects/skhurshid/svt_brady_gwas/phenotypes_wide_032520.RData')
