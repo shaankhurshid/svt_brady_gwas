d9dc0f03116d6b3080778a38a43ec5a7582a5764
sk259 <skhurshid@partners.org>
2021-04-03 14:10:45 -0400

    format

---
+++ format_lz.R
@@ -0,0 +1,18 @@
+# Script to format meta-analysis results for locus zoom
+
+# Depends
+library(data.table)
+
+# List paths
+root <- '/medpop/afib/projects/SVT_BRADY/Preliminary/Meta_analysis/'
+phenos <- c('SVT','WPW_EUR','AVNRT_EUR','SND_INC','SND_REST','DISTAL_INC','DISTAL_REST','PACER_EUR')
+paths <- paste0(root,phenos,'/METAANALYSIS1.TBL')
+
+# Loop and reformat
+for (i in 1:length(paths)){
+  file <- fread(paths[i])
+  splits <- do.call(rbind,strsplit(file$MarkerName,':'))
+  file[,':='(Chr = splits[,1],
+             Genpos = splits[,2])]
+  write.table(file,'/medpop/afib/skhurshid/svt_brady_gwas/meta_outputs/',phenos[i],'.tsv',sep='\t',row.names=F)
+}
